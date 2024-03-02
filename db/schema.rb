dcf91373e7d8b833c7c39d188567e2dfacf9cf10
romain isoz <isozromain@gmail.com>
2024-03-03 00:31:01 +0100

    add user_book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_02_232258) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_232817) do
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -33,10 +33,23 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_232258) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "user_books", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.boolean "read", default: false
+    t.decimal "price", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_user_books_on_book_id"
+    t.index ["user_id"], name: "index_user_books_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_foreign_key "books", "authors"
+  add_foreign_key "user_books", "books"
+  add_foreign_key "user_books", "users"
 end
